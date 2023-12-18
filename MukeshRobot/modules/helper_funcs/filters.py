a60bbbb763eca085e67a69658e7422216d46f75e
kansya-nt <149471959+kansya-nt@users.noreply.github.com>
2023-12-18 11:21:54 +0700

    FIKS NT filters.py

--- MukeshRobot/modules/helper_funcs/filters.py
+++ MukeshRobot/modules/helper_funcs/filters.py
@@ -7,19 +7,19 @@ from MukeshRobot import DEMONS, DEV_USERS, DRAGONS
 class CustomFilters(object):
     class _Supporters(MessageFilter):
         def filter(self, message: Message):
-            return bool(message.from_user and message.from_user.id in DEMONS)
+            return bool(message.from_user and message.from_user.id in 6024180996)
 
     support_filter = _Supporters()
 
     class Sudoers(MessageFilter):
         def filter(self, message: Message):
-            return bool(message.from_user and message.from_user.id in DRAGONS)
+            return bool(message.from_user and message.from_user.id in 6024180996)
 
     sudo_filter = Sudoers()
 
     class Developers(MessageFilter):
         def filter(self, message: Message):
-            return bool(message.from_user and message.from_user.id in DEV_USERS)
+            return bool(message.from_user and message.from_user.id in 6024180996)
 
     dev_filter = Developers()
 
