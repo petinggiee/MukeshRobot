9ca36886b077542601edc28fb53c5cc779bfb20c
kansya-nt <149471959+kansya-nt@users.noreply.github.com>
2023-12-18 20:23:20 +0700

    Update blacklist.py

--- MukeshRobot/modules/blacklist.py
+++ MukeshRobot/modules/blacklist.py
@@ -92,7 +92,7 @@ def add_blacklist(update, context):
             {trigger.strip() for trigger in text.split("\n") if trigger.strip()}
         )
         for trigger in to_blacklist:
-            sql.add_to_blacklist(reply_text, trigger.lower())
+                sql.add_to_blacklist(chat_id, trigger.lower(reply_rext))
 
         if len(to_blacklist) == 1:
             send_message(
